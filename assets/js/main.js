97ccfbca207e7885a6c72c17803937fca4920f05
daoiist <whj.527@gmail.com>
2019-05-23 18:19:44 +0800

    icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(function(){
+	var $window = $(window);
 	//返回顶部
 	var onScroll = function () {
 		var wtop = $window.scrollTop();
@@ -9,5 +10,7 @@ $(function(){
 	    	$('#top').hide();
 		}
 	}
+	$window.scroll(onScroll);
+    onScroll();
 	$('#top').click(function(){document.documentElement.scrollTop=0;document.body.scrollTop=0;return false});
 })
