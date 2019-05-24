f26d1d6208ee4b69e7c2e036247390fcfd7cb1b4
daoiist <whj.527@gmail.com>
2019-05-24 08:48:51 +0800

    top

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,13 @@ $(function(){
 	var onScroll = function () {
 		var wtop = $window.scrollTop();
 		if (wtop > 1200) {
-	   		$('#top').show();
+	   		$('.top').show();
 		}
 		else {
-	    	$('#top').hide();
+	    	$('.top').hide();
 		}
 	}
 	$window.scroll(onScroll);
     onScroll();
-	$('#top').click(function(){document.documentElement.scrollTop=0;document.body.scrollTop=0;return false});
+	$('.top').click(function(){document.documentElement.scrollTop=0;document.body.scrollTop=0;return false});
 })
